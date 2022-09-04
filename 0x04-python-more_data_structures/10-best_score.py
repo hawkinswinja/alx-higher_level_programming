839620ce16cc3ba36511c93bd186ddb8efe9bcf9
hawkinswinja <hawkinswinja@gmail.com>
2022-09-04 08:36:44 +0300

    more data structures

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -3,4 +3,4 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return None
     a = sorted(a_dictionary.items(), key=lambda x: x[1])
-    return (a[-1][1])
+    return (a[-1][0])
