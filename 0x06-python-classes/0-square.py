45de53e9b219458c1b4c16e790daec48216b0167
hawkinswinja <hawkinswinja@gmail.com>
2022-09-13 16:51:40 +0300

    OOP

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
+'''python3 -c 'print(__import__("my_module").__doc__)'''
 class Square:
+    '''python3 -c 'print(__import__("my_module").MyClass.__doc__)'
+	   An empty Square Class.'''
     pass
