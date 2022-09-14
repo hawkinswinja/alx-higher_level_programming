d2a6b0f66c53543d69a55371bd241fc8417888e5
hawkinswinja <hawkinswinja@gmail.com>
2022-09-14 10:50:57 +0300

    getters and setters

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,44 @@
+#!/usr/bin/python3
+"""python3 -c 'print(__import__("my_module").__doc__)'"""
+
+
+class Square:
+    """ python3 -c 'print(__import__("my_module").MyClass.__doc__)'
+        Private instance attribute size. size of the square."""
+    def __init__(self, size=0):
+        """python3 -c 'print(__import__("my_module").my_function.__doc__)'
+           python3 -c 'print(__import__("my_module").\
+           MyClass.my_function.__doc__)'
+           initialize size."""
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        """python3 -c 'print(__import__("my_module").my_function.__doc__)'
+           python3 -c 'print(__import__("my_module").\
+           MyClass.my_function.__doc__)'
+           returns size."""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """python3 -c 'print(__import__("my_module").my_function.__doc__)'
+           python3 -c 'print(__import__("my_module").\
+           MyClass.my_function.__doc__)'
+           sets size"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """python3 -c 'print(__import__("my_module").my_function.__doc__)'
+           python3 -c 'print(__import__("my_module").\
+           MyClass.my_function.__doc__)'
+           Returns the Area of the square."""
+        return self.__size**2
