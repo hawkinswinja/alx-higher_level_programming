4682ca83c87c38faf39abf7324408224a273e28e
hawkinswinja <hawkinswinja@gmail.com>
2022-09-26 18:23:54 +0300

    inheritance in python

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
 """ 9-base_geometry module
     class with defined function
 """
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
