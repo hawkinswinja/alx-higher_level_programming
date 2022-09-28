4cfea4bc4156a6d69e14ebc71b08c6e076503c11
hawkinswinja <hawkinswinja@gmail.com>
2022-09-28 20:46:15 +0300

    class instances

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""this module defines class student with one function"""
+
+
+class Student:
+    """class representation of student details"""
+
+    def __init__(self, first_name, last_name, age):
+        """initialization of public variables"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """retrieves the json dict of the student"""
+        if type(attrs) is list and len(attrs) != 0:
+            nlist = []
+            for attr in attrs:
+                x = self.__dict__.get(attr)
+                if x is not None:
+                    nlist.append((attr, x))
+            return dict(nlist)
+        else:
+            return self.__dict__
+
+    def reload_from_json(self, json):
+        """replace all attributes of the student instance with new values"""
+        self.__dict__ = json
