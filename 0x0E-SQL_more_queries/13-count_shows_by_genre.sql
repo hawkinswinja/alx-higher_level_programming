3b96af5cd52e056a868ee1e8eb809ebcd2296b08
hawkinswinja <hawkinswinja@gmail.com>
2022-11-04 05:03:15 -0700

    correction of 13 and 16

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,5 +1,5 @@
 -- count number of shows by genre
-SELECT name as 'genre', count(*) as 'number_of_shows'
+SELECT name AS 'genre', count(*) AS 'number_of_shows'
 FROM tv_genres
 INNER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
