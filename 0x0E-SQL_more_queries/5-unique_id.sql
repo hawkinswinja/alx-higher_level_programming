89087c6b2c5eac2b620be7ab7825b5fa3d2cb6c5
hawkinswinja <hawkinswinja@gmail.com>
2022-11-04 02:37:48 -0700

    unique, primary and foreign keys

---
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -0,0 +1,2 @@
+-- create table with unique attributes ID
+CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,4 @@
+-- create database states with table
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (id INT AUTO_INCREMENT PRIMARY KEY, state_id INT NOT NULL, name VARCHAR(256) NOT NULL, 
+FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id));
