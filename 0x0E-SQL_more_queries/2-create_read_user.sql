1c5d7d0c62b824029ff94d721473e3888c583c3f
hawkinswinja <hawkinswinja@gmail.com>
2022-11-04 01:55:36 -0700

    correction for 2

--- 0x0E-SQL_more_queries/2-create_read_user.sql
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -1,5 +1,4 @@
 -- create user with select privileges
 CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
 CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
-GRANT SELECT ON hbtn_od_2.* TO 'user_0d_2'@'localhost';
-FLUSH PRIVILEGES;
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
