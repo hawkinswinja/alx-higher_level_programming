8e4b31a4624805650d05e5a1219f5509a73797e6
hawkinswinja <104626373+hawkinswinja@users.noreply.github.com>
2023-01-17 17:21:35 +0300

    Update 4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,15 +1,14 @@
 #!/usr/bin/node
 const request = require('request');
-
 request(process.argv[2], function (error, response, body) {
   if (error) { console.log(error); }
   if (response.statusCode === 200) {
     const movies = JSON.parse(body).results;
     let appeared = 0;
-    for (let i = 0; i < movies.length; i++) {
-      if (movies[i].characters.find(function (id) {
-        return (id === 'https://swapi-api.alx-tools.com/api/people/18/');
-      })) { appeared++; }
+    for (const movie of movies) {
+      for (const charURL of movie.characters) {
+        if (charURL.includes(18)) { appeared++; }
+      }
     }
     console.log(appeared);
   }
