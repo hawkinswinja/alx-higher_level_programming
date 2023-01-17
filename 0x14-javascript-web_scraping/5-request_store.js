1ebcfdf9bd220f5e86e16d15f285b3b27be4f788
hawkinswinja <104626373+hawkinswinja@users.noreply.github.com>
2023-01-17 17:14:15 +0300

    Update 5-request_store.js

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,10 +1,5 @@
 #!/usr/bin/node
 const request = require('request');
-const fs = require('fs').promises;
+const fs = require('fs');
 // write the body to file
-request.get(process.argv[2], function (error, response, body) {
-  if (error) { console.log(error); }
-  if (body) {
-    fs.writeFile(process.argv[3], body, 'UTF8');
-  }
-});
+request.get(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
